c085c89e2b0cd2f736d3199bc532e138b25f389b
Miten Kashiyani <miten.kashiyani96@gmail.com>
2020-05-21 18:48:47 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 var name = "susan"
 var height = "74"
 
-if(name === "susan") {
-  console.log("The name is correct")
-}
-else {
-  console.log("Expected the name to be Susan");
-}
-
 // Don't worry about this
 module.exports = { name, height
 }
